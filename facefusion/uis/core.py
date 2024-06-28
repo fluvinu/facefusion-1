1495889e7780999b838d248becca1359bb9b6c28
Siddharth Shivwanshi <65756060+fluvinu@users.noreply.github.com>
2024-06-29 00:40:21 +0530

    Update core.py

--- facefusion/uis/core.py
+++ facefusion/uis/core.py
@@ -154,3 +154,4 @@ def get_css() -> str:
 	fixes_css_path = resolve_relative_path('uis/assets/fixes.css')
 	overrides_css_path = resolve_relative_path('uis/assets/overrides.css')
 	return open(fixes_css_path, 'r').read() + open(overrides_css_path, 'r').read()
+ 
